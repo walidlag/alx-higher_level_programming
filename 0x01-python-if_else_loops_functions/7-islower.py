cbb53bc485ee8ff73a1a0c284eb2adbbac33fc05
walidlag <walidlagmah8@gmail.com>
2023-11-29 14:54:04 -0800

    234

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
-User
 #!/usr/bin/python3
 def islower(c):
-    """Check if a character is lowercase."""
-    return ord('a') <= ord(c) <= ord('z')
+    if ord(c) <= ord('z') and ord(c) >= ord('a'):
+        return True
+    else:
+        return False
