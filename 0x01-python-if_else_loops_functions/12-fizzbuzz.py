96857d801adcc9ee1beba6278ac34e520c935354
walidlag <walidlagmah8@gmail.com>
2023-11-29 15:39:50 -0800

    3443

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 15 == 0:
+        if q % 15 == 0:
             print("FizzBuzz", end=" ")
-        elif number % 3 == 0:
+        elif q % 3 == 0:
             print("Fizz", end=" ")
-        elif number % 5 == 0:
+        elif q % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(i, end=" ")
+            print(q, end=" ")
