73d9d81b02793fa039ce24ff6d42004fe6e98472
walidlag <walidlagmah8@gmail.com>
2023-12-06 16:05:26 -0800

    34

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pythoni3
+def square_matrix_simple(matrix):
+    result = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(element ** 2)
+        result.append(new_row)
+    return result
