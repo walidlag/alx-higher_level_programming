0b1037fa79b055eca7a5333a310ae849f1cfadda
walidlag <walidlagmah8@gmail.com>
2023-12-18 15:29:51 -0800

    exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
      try:
-        print('{:d}'.format(value))
-        return True
-    except (ValueError, TypeError):
+        print("{:d}".format(value))
+    except:
         return False
+    else:
+        return True
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-     q, count = 0, 0
+     q, c = 0, 0
     while q < x:
         try:
             print("{:d}".format(my_list[q]), end='')
-            count += 1
+            c += 1
         except (TypeError, ValueError):
             pass
         q += 1
     print()
-    return count
+    return c
