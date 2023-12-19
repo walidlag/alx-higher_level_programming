255013383ae8f1f23cebe2da967c66bba0d2926e
walidlag <walidlagmah8@gmail.com>
2023-12-19 15:34:06 -0800

    34

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,29 +4,23 @@
 
 class Square:
     """Defines a class aquare."""
+
     def __init__(self, size=0):
-        """ init square
-
-        Args:
-            value (int): size of the square.
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """initialize square
-
-        Returns:
-            Private size.
-        """
-        return self.__size
-
-    @size.setter
-     def __init__(self, size=0):
         """initialize square
         Args:
             size (int): size of the square
         """
+        self.__size = size
+
+    @property
+    def size(self):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        return self.__size
+
+    @size.setter
         if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
