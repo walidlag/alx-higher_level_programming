ca9b77448dfed3fcd86d3d11375b0344fcabe1c2
walidlag <walidlagmah8@gmail.com>
2023-12-18 16:11:56 -0800

    543

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    c = 0
+    count = 0
     try:
         for q in range(x):
             if type(my_list[q]) is int:
@@ -10,4 +10,4 @@ def safe_print_list_integers(my_list=[], x=0):
         return
     else:
         print()
-        return c
+        return count
