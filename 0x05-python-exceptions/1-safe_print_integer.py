af95f131374de8a133045bd546ec1641e2a43452
walidlag <walidlagmah8@gmail.com>
2023-12-18 16:14:07 -0800

    3222

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-     try:
+    try:
         print("{:d}".format(value))
     except:
-        return False
+        return (False)
     else:
-        return True
+        return (True)
