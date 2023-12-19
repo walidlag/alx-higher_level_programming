2c6866ed7f58a5dea5156f9627a9cffe06c1c85a
walidlag <walidlagmah8@gmail.com>
2023-12-19 13:42:26 -0800

    walidlag

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,13 +2,13 @@
 """square module for python 0x06."""
 
 
-class Square:
-    """Define a Square."""
-     
-    def __init__(self, size):
-        """Initialize a new Square.
+class square:
+    """Defines a square."""
+    
+    def _init_(self, size):
+        """Constructor.
 
         Args:
-            size : length of a side of the square.
-        """
-        self.__size = size
+            size: length of a side of the square.
+            """
+            self.__size = size
