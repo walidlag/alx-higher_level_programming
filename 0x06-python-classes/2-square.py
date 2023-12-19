6fe7d13654be0ae1b40c725cf6523cc839c58aee
walidlag <walidlagmah8@gmail.com>
2023-12-19 14:50:02 -0800

    walidlag

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class aquare."""
+
+
+class Square:
+    """Defines a class aquare."""
+
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
