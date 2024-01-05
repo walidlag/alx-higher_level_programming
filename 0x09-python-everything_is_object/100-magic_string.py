69154e67984afe5c719e71c28b4828cb74dd6b21
walidlag <walidlagmah8@gmail.com>
2024-01-05 15:40:06 -0800

    walidla

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(number):
-    result = 'BestSchool, ' * (number - 1) + 'BestSchool'
+def magic_string(n):
+    result = 'BestSchool, ' * (n - 1) + 'BestSchool'
     return result
