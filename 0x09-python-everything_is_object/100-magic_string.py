5b14ce3899a4a5b9d36560474c5bba3bb4f08062
walidlag <walidlagmah8@gmail.com>
2024-01-05 15:34:04 -0800

    walidlag

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(number):
+    result = 'BestSchool, ' * (number - 1) + 'BestSchool'
+    return result
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,2 @@
+0x09-python-everything is object
+0x09. Python - Everything is object
