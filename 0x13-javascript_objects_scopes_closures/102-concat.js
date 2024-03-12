79b917d88d313ff2d63746e64d5d13189e4abf12
walidlag <walidlagmah8@gmail.com>
2024-03-12 15:25:16 -0700

    waal

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const dataA = fs.readFileSync(fileA, { encoding: 'utf8' });
+const dataB = fs.readFileSync(fileB, { encoding: 'utf8' });
+fs.writeFileSync(fileC, dataA + dataB, { encoding: 'utf8' });
