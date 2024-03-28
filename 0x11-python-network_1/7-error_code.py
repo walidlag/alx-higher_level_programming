dcffcc3609fc4d46e3f03e33a1c447daa9169409
walidlag <walidlagmah8@gmail.com>
2024-03-28 14:02:41 -0700

    walidlag

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,15 +2,13 @@
 """
 The script will send a request to the given URL, showing the response body if there are no issues and indicating the HTTP error code otherwise
 """
-    from sys import argv
-    from requests import get
+import requests
+import sys
 
 
 if __name__ == "__main__":
-
-    url = argv[1]
-
-    response = get(url)
-    ERR_TXT = 'Error code: {}'
-    status = response.status_code
-    print(ERR_TXT.format(status) if (status >= 400) else response.text)
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
