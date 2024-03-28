e0f266749f19c50cef130eb776938f46a74893cc
walidlag <walidlagmah8@gmail.com>
2024-03-28 14:51:40 -0700

    walidlag

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """The Python script retrieves a specific variable's value from the response header after sending a request to the given URL
 """
-from requests import get
 from sys import argv
+import requests
 
 
 if __name__ == "__main__":
-    repo = argv[1]
-    owner = argv[2]
-
-    URL = f"https://api.github.com/repos/{owner}/{repo}/commits"
-
-    response = get(URL)
-    commits = response.json()
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
 
     try:
-        for commit in commits[:10]:
-            sha = commit.get("sha")
-            author_name = commit.get("commit").get("author").get("name")
-            print(f"{sha}: {author_name}")
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
     except IndexError:
-        print("Less than 10 commits available.")
+        pass
