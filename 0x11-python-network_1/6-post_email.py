13e704920aeacde5fe47d394940cdc31c1ba970b
walidlag <walidlagmah8@gmail.com>
2024-03-28 14:44:41 -0700

    walidlag

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
 Initiates a POST request to the provided URL, including the email as a parameter, and then showcases the response body"""
-from requests import post
+import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
-    email = argv[2]
-    res = post(url, {'email': email})
-    print(res.text)
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
